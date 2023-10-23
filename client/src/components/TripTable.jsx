b3d86f506dd00fd583f5f7610b890c7f471fdeb8
Andrew Brown <140449662+andy-brown87@users.noreply.github.com>
2023-10-23 21:25:22 +0100

    added delete button, not working yet though

--- client/src/components/TripTable.jsx
+++ client/src/components/TripTable.jsx
@@ -1,6 +1,18 @@
 // import TransportMode from "./TransportMode"
 
-const TripTable = (mode) => {
+const TripTable = ({mode, deleteTrip, removeTrip}) => {
+const [modeTrips, setModeTrips] = useState([])
+    
+    const handleDelete = () => {
+        deleteTrip(trip._id).then(() => {
+            removeTrip(trip._id)
+        })
+
+    const removeTrip = (id) => {
+        const tripToKeep = modeTrips.filter(trip => trip._id !== id)
+        setModeTrips(tripToKeep)
+        }
+
     return (
         <section>
             <p>You company's total emissions</p> 
@@ -22,13 +34,15 @@ const TripTable = (mode) => {
                     <td>*distance*</td>
                     <td>{mode.emissions}</td>
                     <td><button>Update</button></td>
-                    <td><button>Delete</button></td>
+                    <td><button onClick={handleDelete}>Delete</button></td>
+                    
+    
 
                 </tr> 
             </table>
         </section>
         // <h1>I'm the TripTable</h1>
-    )
-}
+    )}
+    }
 
 export default TripTable

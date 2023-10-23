1ba8df389778f038613c1b3d843b97d4a07792d1
LidzDev <lydia.cordell@gmail.com>
2023-10-23 15:22:52 +0100

    added employee dropdown to the form and added transport id

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
-// import TransportList from './components/TransportList'
 import MainTitle from './components/MainTitle'
 import TransportService from './services/TransportService'
+import EmployeeService from './services/EmployeeService'
 import './App.css'
 import TripForm from './components/TripForm'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -9,13 +9,17 @@ import NavBar from './components/NavBar'
 
 function App() {
   const [modes, setModes] = useState([])
+  const [employees, setEmployees] = useState([])
   const title = "CO2 Calculator"
   const intro = "Calculate your carbon footprint"
 
   useEffect(() => {
     TransportService.getModesOfTransport()
       .then(modes => setModes(modes))
+      .then(EmployeeService.getEmployees()
+        .then(employees => setEmployees(employees)))
     console.log(modes)
+    console.log(employees)
   }, [])
   return (
     <Router>
@@ -24,7 +28,7 @@ function App() {
       <div className="flex-box">
         <div className="calc-form">
           <Routes>
-            <Route path="/" element={<TripForm modes={modes} />} />
+            <Route path="/" element={<TripForm modes={modes} employees={employees} />} />
             {/* <Route path="/triptable" element={<TripTable />} /> */}
           </Routes>
         </div>

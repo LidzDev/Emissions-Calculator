1efd13382745011a7488094ccccdd8d691698f6c
Claire Martin <claire.e.martin15@gmail.com>
2023-10-24 17:18:36 +0100

    some more work on the nav bar

--- client/src/components/TripEmissionsTotal.jsx
+++ client/src/components/TripEmissionsTotal.jsx
@@ -4,7 +4,7 @@ const TripEmissionsTotal = () => {
             <p>*this prob needs to be a seperate jsx</p>
             <p>Estimated CO2 emissions: </p>
             <p className="trip-emissions-total">50.93 kg</p> {/**this will be where the calc for the trip goes* */}
-            <button className="log-estimate-btn" >Log estimate</button>
+            {/* <button className="log-estimate-btn" >Log estimate</button> */}
             <p>View total estimated CO2 emissions <br></br> *links to entries page*</p>                
         </div>
     );

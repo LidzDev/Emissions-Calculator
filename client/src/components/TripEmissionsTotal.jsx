2f5ea89e704ed64648a1ad3eafcba3fb465d0191
darren lackie <lackiewwpj@gmail.com>
2023-10-25 16:55:04 +0100

    NaN no longer displaying when no values are selected

--- client/src/components/TripEmissionsTotal.jsx
+++ client/src/components/TripEmissionsTotal.jsx
@@ -1,14 +1,15 @@
 const TripEmissionsTotal = ({tripEmissions}) => {
 
     const emissionsKg = (tripEmissions / 1000)
+    const checkingEmissionsKg = isNaN(emissionsKg) ? 0 : emissionsKg
 
     return ( 
         <div className="form-output">
             <h2><b>Estimated CO2 emissions: </b></h2>
-                <p className="trip-emissions-total">{emissionsKg} kg</p>
+                <p className="trip-emissions-total">{checkingEmissionsKg} kg</p>
                 <br></br>
                 <a href="/triptable" className="results-link">View total estimated CO2 emissions</a>
-         </div>
+        </div>
     );
 }
 
